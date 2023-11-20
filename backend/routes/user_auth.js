dcb144b9cb46a155f2ba8edc3d96b3803bf799ff
Muhammad Sarmad Saleem <sarmadsaleem.333@gmail.com>
2023-11-20 18:45:15 +0500

    Added more precise tables

--- backend/routes/user_auth.js
+++ backend/routes/user_auth.js
@@ -12,10 +12,11 @@ const fetchuser = require('../midlleware/fetchuser');
 router.post("/create_user", [
     body("name", "Enter a valid name").isLength({ min: 5 }),
     body("email", "Enter a valid email").isEmail(),
-    body("phone", "Enter a valid phone number").isLength({ max: 11 }),
-    body("password", "Password should be atleast 8 characters").isLength({ min: 8 })
+    body("password", "Password should be atleast 8 characters").isLength({ min: 8 }),
+    body("status","Enter your status").notEmpty(),
+    body("phone","Phone number should be of 11 characters").isLength(11),
 ], async (req, res) => {
-    const { name, email, phone, password } = req.body;
+    const { name, email,  password,status,phone } = req.body;
     let success = false;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -24,8 +25,9 @@ router.post("/create_user", [
     }
     try {
         // Check if a user with the same email or phone already exists
-        con.query("SELECT * from users WHERE email = ? or phone = ?", [email, phone], (error, results) => {
+        con.query("SELECT * from users WHERE uemail = ? or uphone=? " , [email,phone],(error, results) => {
             if (error) {
+                console.log(error)
                 console.log("Error finding user");
                 return res.status(500).json({ error: "Internal server error", success });
             }
@@ -33,6 +35,7 @@ router.post("/create_user", [
             if (results.length > 0) {
                 return res.status(400).send("A user with this email or phone already exists. Please use another email or phone.");
             }
+
 
             // Hash the password using bcrypt.js
             bcrypt.genSalt(10, (err, salt) => {
@@ -48,7 +51,7 @@ router.post("/create_user", [
                     }
 
 
-                    con.query("INSERT INTO users (name, email, phone, password) VALUES (?, ?, ?, ?)", [name, email, phone, secPass], (error, result) => {
+                    con.query("INSERT INTO users (uname, uemail,ustatus, upassword,uphone) VALUES (?,?,?,?,?)", [name, email, status, secPass,phone], (error, result) => {
                         if (error) {
                             console.log("Error creating a user", error);
                             return res.status(500).json({ error: "Internal server error", success });
@@ -86,7 +89,7 @@ router.post("/login_user", [
         return res.status(400).json(response[0].msg);
     }
     try {
-        con.query("Select * from users where email =? limit 1", [email], async (error, user) => {
+        con.query("Select * from users where uemail =? limit 1", [email], async (error, user) => {
             if (error) {
                 return res.send("Error finding user");
             }
@@ -94,13 +97,13 @@ router.post("/login_user", [
 
                 return res.json({ message: "No user found with this email", success });
             }
-            const passwordCompare = await bcrypt.compare(password, user[0].password);
+            const passwordCompare = await bcrypt.compare(password, user[0].upassword);
             if (!passwordCompare) {
                 return res.status(400).json({ success, error: "Please enter the correct Password" });
             }
             const data = {
                 user: {
-                    id: user[0].user_id
+                    id: user[0].uno
                 }
             };
             const authtoken = jwt.sign(data, JWT_Secret);
@@ -118,12 +121,13 @@ router.post("/login_user", [
 //fetch user details here 
 router.get("/fetch_user", fetchuser, async (req, res) => {
     try {
-        con.query("select name,email,phone from users where user_id=?", [req.user.id], (error, result) => {
+        con.query("select uname,uemail,ustatus, uphone from users where uno=?", [req.user.id], (error, result) => {
 
             if (error) {
                 return res.send("Error fetching details");
             }
-            res.send(result[0]);
+             console.log(req.user.id)
+             res.send(result[0]);
         }
         )
     } catch (error) {
