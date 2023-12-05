29ddf0d85f020d6c8d88f24ed7073ebbc9c0ddf0
Muhammad Sarmad Saleem <sarmadsaleem.333@gmail.com>
2023-12-05 20:43:12 +0500

    Made fixes in history

--- src/components/HistoryItem.js
+++ src/components/HistoryItem.js
@@ -3,12 +3,12 @@ import React from 'react'
 export default function HistoryItem() {
     return (
 
-        <div className='flex justify-evenly py-3 px-0'>
-            <div className=' border-gray-900 text-gray-900'>Funding</div>
+        <div className='flex justify-between py-3 '>
+            <div className=' border-gray-900 text-gray-900'>Fundingwdbd wikd wj</div>
             <div className=' border-gray-900 text-gray-900'>23-09-2023</div>
             <div className=' border-gray-900 text-gray-900'>400</div>
-            <div className=' border-gray-900 text-gray-900 '>234567890</div>
-            <div className=' border-gray-900 text-gray-900'>Sarmad</div>
+            <div className=' border-gray-900 text-gray-900  '>234567890wdbwj d</div>
+            <div className=' border-gray-900 text-gray-900 '> Sarmad Saleem </div>
         </div>
 
     )
--- src/components/UserHistory.js
+++ src/components/UserHistory.js
@@ -7,7 +7,7 @@ export default function UserHistory() {
       <h2 className="text-2xl font-bold leading-7 pt-10 text-center text-black-900 sm:truncate sm:text-3xl sm:tracking-tight">
         Your Donation History is as Follows
       </h2>
-      <div className='flex justify-evenly py-3'>
+      <div className='flex justify-between py-3'>
         <div className='border-b border-r border-gray-900 text-red-900 font-bold sm:truncate sm:text-3xl sm:tracking-tight'>Case Name</div>
         <div className='border-b border-r border-gray-900 text-red-900 font-bold sm:truncate sm:text-3xl sm:tracking-tight'>Date of Donation</div>
         <div className='border-b border-r border-gray-900 text-red-900 font-bold sm:truncate sm:text-3xl sm:tracking-tight'>Amount Donated </div>
