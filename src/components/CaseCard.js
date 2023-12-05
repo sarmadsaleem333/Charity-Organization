6f8c7ea8743402eef03300c73d6c4fc21986cc26
Muhammad Sarmad Saleem <sarmadsaleem.333@gmail.com>
2023-12-05 19:45:27 +0500

    Completed donatation frontend

--- src/components/CaseCard.js
+++ src/components/CaseCard.js
@@ -2,28 +2,60 @@ import React from 'react';
 
 export default function CaseCard() {
     return (
-        <div className='py-8 px-8'>
-            <div className="max-w-md rounded overflow-hidden shadow-lg border-2 border-gray-300 p-4">
-                <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">Student Fee issue</div>
-                    
-                    <p className="text-gray-700 text-base">
-                        Your generous contributions make a difference in the lives of those facing hardships. Let's come together and create positive change in the world.
-                    </p>
-                </div>
-                <div className='flex justify-evenly'>
-                <dd className="order-first text-3xl font-semibold tracking-tight text-red-500 sm:text-4xl">
-                PKR 100 
-                </dd>
-                <p className='py-2 font-semibold'>Last Date: 5-December-2023</p>
-                </div>
-                <div className="px-6 pt-4 pb-2 flex justify-between">
-                    <a href="#" className="block mt-2  font-semibold text-blue-500 text-1xl hover:underline">Read more</a>
-                    <button type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Donate</button>
+        <>
+            {/* modal */}
+            <div className="modal fade" id="modal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h3 className="modal-title" id="exampleModalLabel">Donating for case </h3>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Enter your Account No</label>
+                                <input type="name" class="form-control" id="exampleInputEmail1 " />
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Enter your account name</label>
+                                <input type=" name" class="form-control" id="exampleInputPassword1" />
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Enter amount(Atleast Rs.100)</label>
+                                <input type=" name" class="form-control" id="exampleInputPassword2" />
+                            </div>
+                        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Donate</button>
+                        </div>
+
+
+                    </div>
                 </div>
             </div>
 
-        </div>
+            <div className='py-8 px-8'>
+                <div className="max-w-md rounded overflow-hidden shadow-lg border-2 border-gray-300 p-4">
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">Student Fee issue</div>
+                        <p className="text-gray-700 text-base">
+                            Your generous contributions make a difference in the lives of those facing hardships. Let's come together and create positive change in the world.
+                        </p>
+                    </div>
+                    <div className='flex justify-evenly'>
+                        <dd className="order-first text-3xl font-semibold tracking-tight text-red-500 sm:text-4xl">
+                            PKR 100
+                        </dd>
+                        <p className='py-2 font-semibold'>Last Date: 5-December-2023</p>
+                    </div>
+                    <div className="px-6 pt-4 pb-2 flex justify-between">
+
+
+                        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"  data-bs-toggle="modal"
+                            data-bs-target="#modal">Donate</button>
+                    </div>
+                </div>
+
+            </div>
+        </>
     );
 }
-                    
+
